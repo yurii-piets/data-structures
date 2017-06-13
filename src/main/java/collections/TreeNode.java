235fa34e5ea1ae062a1a779f7ba17d1b06bfe7e2
Yurii Piets <yurii.piets@gmail.com>
2017-06-13 21:05:25 +0200

    TreeNode

---
+++ src/main/java/collections/TreeNode.java
@@ -0,0 +1,16 @@
+package collections;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TreeNode<T> implements Node{
+    private T value;
+    private TreeNode<T> left;
+    private TreeNode<T> right;
+
+    public TreeNode(T value) {
+        this.value = value;
+    }
+}
