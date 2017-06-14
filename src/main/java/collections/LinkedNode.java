3c173ff69b85d1b4a53d032488ec866821455921
Yurii Piets <yurii.piets@gmail.com>
2017-06-14 22:56:39 +0200

    Small code cleanup

--- src/main/java/collections/LinkedNode.java
+++ src/main/java/collections/LinkedNode.java
@@ -1,12 +1,11 @@
 package collections;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class LinkedNode<T> {
+public class LinkedNode<T> implements Node<T> {
     private T element;
     private LinkedNode<T> next;
     private LinkedNode<T> previous;
